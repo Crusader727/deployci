d136c9e3136553f46532949db9427c0804f0550d
Crusader727 <rubenhov97@gmail.com>
2021-08-29 14:28:20 +0300

    ddd

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          new main111
+          test new build with dirs
         </a>
       </header>
     </div>
