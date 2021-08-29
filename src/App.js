147a37d64ede53dbfe21dd1c601d739bd0341164
Crusader727 <rubenhov97@gmail.com>
2021-08-29 14:40:04 +0300

    dd

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          test new build with dirs
+          new branch test!!!
         </a>
       </header>
     </div>
