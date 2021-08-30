c110311cc8fcc625bc8af78b9ba8147be9a3878e
Crusader727 <rubenhov97@gmail.com>
2021-08-30 17:40:54 +0300

    hmmm

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          ggggggggggddddd
+          ggggggggggddddddd
         </a>
       </header>
     </div>
